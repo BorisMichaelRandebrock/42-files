4dd7b8dd61dcb32af52075da0b87011862370d16
Boris Randebrock <brandebr@car14s2.42barcelona.com>
2023-04-07 17:29:34 +0200

    exercise C-02 ex00

---
+++ C-02/ex00/ft_strcpy.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brandebr <brandebr@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/07 10:11:32 by brandebr          #+#    #+#             */
+/*   Updated: 2023/04/07 17:11:44 by brandebr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+/*
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != 0)
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(void)
+{
+	char	str[27] = "My name is Inigo Montoya..";
+	char	dest[27];
+
+	ft_strcpy(dest, str);
+	ft_putstr(str);
+}*/
