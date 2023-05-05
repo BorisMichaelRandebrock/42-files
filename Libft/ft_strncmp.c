72711b93bead8a67f79e729da6a24076adfe573a
Boris Randebrock <brandebr@cbr13s1.42barcelona.com>
2023-05-05 16:17:34 +0200

    ft_strcmp 🐥

---
+++ Libft/ft_strncmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brandebr <brandebr@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/05 12:37:52 by brandebr          #+#    #+#             */
+/*   Updated: 2023/05/05 14:19:45 by brandebr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+	size_t	diff;
+
+	i = 0;
+	while ((s1[i] || s2[i]) && n > i)
+	{
+		if (s1[i] != s2[i])
+		{
+			diff = (unsigned char)s1[i] - (unsigned char)s2[i];
+			return (diff);
+		}
+		i++;
+	}
+	return (0);
+}
