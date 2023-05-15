f61fb131015d5bd7a66853d9ac17814d767f7a4a
Boris Michael Randebrock <boris@randebrock.com>
2023-05-15 22:58:18 +0200

    fine tuning strjoin & mmmove

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -12,29 +12,29 @@
 
 #include "libft.h"
 
-char    *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-    char    *str;
-    int     i;
-    int     j;
+	int	i;
+	int	j;
+	char	*str;
 
-    i = 0;
-    j = 0;
-    if (!s1 || !s2)
-        return (NULL);
-    str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-    if (!str)
-        return (NULL);
-    while (s1[i])
-    {
-        str[i] = s1[i];
-        i++;
-    }
-    while (s2[j])
-    {
-        str[i + j] = s2[j];
-        j++;
-    }
-    str[i + j] = '\0';
-    return (str);
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
 }
