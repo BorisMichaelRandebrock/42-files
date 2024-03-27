10b9dbdd1ae9aa4bd1b784b924314abb9db45788
Boris Randebrock <brandebr@car3s2.42barcelona.com>
2024-03-27 18:33:52 +0100

    jee

--- rush00/ex00/main.c
+++ rush00/ex00/main.c
@@ -6,26 +6,41 @@
 /*   By: ereinald <ereinald@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/01 16:27:41 by ereinald          #+#    #+#             */
-/*   Updated: 2023/04/02 18:37:07 by brandebr         ###   ########.fr       */
+/*   Updated: 2024/03/27 17:36:50 by brandebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	rush00(int x, char y);
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+void	rush00(char x, char y);
 void	ft_putchar(char c);
 void	rush01(char x, char y);
 void	rush03(char x, char y);
 void	rush02(char x, char y);
 void	rush04(char x, char y);
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	rush00(3, 5);
+	int	ar;
+	int	gv;
+
+	if (argc != 3)
+		return (printf("wrong amount of arguments"));
+	ar = atoi(argv[1]);
+	gv = atoi(argv[2]);
+	if (ar < 0 || gv < 0)
+		return (printf("numbers can not be negative...."));
+	rush00(ar, gv);
 	ft_putchar('\n');
-	rush01(4, 5);
+	rush01(ar, gv);
 	ft_putchar('\n');
-	rush02(5, 6);
+	rush02(ar, gv);
 	ft_putchar('\n');
-	rush03(9, 6);
+	rush03(ar, gv);
 	ft_putchar('\n');
-	rush04(5, 7);
+	rush04(ar, gv);
+	return (0);
 }
